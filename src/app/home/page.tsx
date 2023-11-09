c166ef9b927224873398546b57396f2743150ac1
yuki-1184 <yuki.1184apple@gmail.com>
2023-11-09 10:56:11 +0900

    css-in-js registry

---
+++ src/app/home/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import styled from 'styled-components';
+
+type Props = {};
+const Home = () => {
+  return (
+    <HelloWorld>
+      <p>hello world</p>
+    </HelloWorld>
+  );
+};
+
+const HelloWorld = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+`;
+
+export default Home;
